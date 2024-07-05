b34ae8d3349f6b592d52ac0b444e5ae46db07215
walter.netoo <walter.neto@kodigos.com.br>
2024-07-05 10:45:24 -0400

    :sparkles: feat: Enroll Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/homePage/Hero";
 import StatisticsSection from "@/components/homePage/statisticsSection";
 import Blog from "@/components/homePage/Blog";
 import Team from "@/components/homePage/Team";
+import Enroll from "@/components/homePage/Enroll";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <StatisticsSection />
       <Blog />
       <Team />
+      <Enroll />
     </div>
   );
 }
