6cf91039a5501419dd8d86a91e6aceffb82b3d5d
walter.netoo <walter.neto@kodigos.com.br>
2024-07-05 10:16:35 -0400

    :sparkles: feat: Team Component

--- src/components/homePage/Team.tsx
+++ src/components/homePage/Team.tsx
@@ -2,32 +2,27 @@ import Image from "next/image";
 
 const people = [
   {
-    imageUrl:
-      "/perfil2.png",
+    imageUrl: "/perfil2.png",
   },
   {
-    imageUrl:
-      "/perfil1.png",
+    imageUrl: "/perfil1.png",
   },
   {
-    imageUrl:
-      "/perfil5.png",
+    imageUrl: "/perfil5.png",
   },
   {
-    imageUrl:
-      "/perfil3.png",
+    imageUrl: "/perfil3.png",
   },
   {
-    imageUrl:
-      "/perfil4.png",
+    imageUrl: "/perfil4.png",
   },
 ];
 
 export default function Team() {
   return (
     <div className="bg-white relative mb-20">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8 flex justify-center relative z-10">
-        <ul role="list" className="flex gap-x-2">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 flex flex-col items-center relative z-10">
+        <ul role="list" className="flex gap-x-2 sm:gap-x-4 -mb-2 z-10">
           {people.map((person, index) => (
             <li key={index} className="flex-shrink-0">
               <div className="flex items-center justify-center">
@@ -36,17 +31,17 @@ export default function Team() {
                   src={person.imageUrl}
                   width={80}
                   height={80}
-                  className="h-16 w-16 rounded-full"
+                  className="h-14 w-14 rounded-full"
                 />
               </div>
             </li>
           ))}
         </ul>
-      </div>
-      <div className="flex justify-center mt-9 relative z-0">
-        <button className="flex items-center bg-customBlue text-white text-xl font-semibold py-5 px-9 rounded-xl shadow-lg hover:bg-customBlue2 focus:outline-none focus:ring-2 focus:ring-customBlue2 focus:ring-offset-2 -mt-12">
-          Saiba mais detalhes sobre os nossos palestrantes
-        </button>
+        <div className="flex justify-center relative z-0">
+          <button className="flex items-center bg-customBlue text-white text-xl font-semibold py-4 px-9 rounded-xl shadow-lg hover:bg-customBlue2 focus:outline-none focus:ring-2 focus:ring-customBlue2 focus:ring-offset-2">
+            Saiba mais detalhes sobre os nossos palestrantes
+          </button>
+        </div>
       </div>
     </div>
   );
